6490c5239060c77c24f2c6288c1cb7a818cdd63c
Justyna Krotoszynska <justyna.krotoszynska@gmail.com>
2021-03-29 17:18:44 +0200

    zadanie 1

--- lab02zad01/src/com/company/Main.java
+++ lab02zad01/src/com/company/Main.java
@@ -3,6 +3,46 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        class rower
+     Rower rowerPierwszy = new Rower();
+     Rower rowerDrugi = new Rower();
+
+     rowerPierwszy.start();
+     rowerDrugi.start();
+
+     rowerDrugi.przyspiesz(50);
+
+     rowerPierwszy.wyswietlStan();
+     rowerDrugi.wyswietlStan();
+
+     rowerDrugi.stop();
+
+     rowerDrugi.wyswietlStan();
+
+    }
+}
+
+class Rower {
+    boolean wRuchu;
+    int predkosc;
+
+
+    public void start() {
+        wRuchu = true;
+        predkosc = 0;
+    }
+
+    public void stop() {
+        wRuchu = false;
+        predkosc = 0;
+    }
+
+    public void przyspiesz(int nowaPredkosc) {
+        predkosc = nowaPredkosc;
+    }
+
+    public void wyswietlStan() {
+        System.out.println("Obecny stan");
+        System.out.println("Prędkość wynosi " + predkosc);
+        System.out.println("Czy rower jest w ruchu?" + (wRuchu ? " Tak" : " Nie"));
     }
 }
