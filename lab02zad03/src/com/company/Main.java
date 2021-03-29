68927002d55847e4ef079baf5e29e554ca7c7990
Justyna Krotoszynska <justyna.krotoszynska@gmail.com>
2021-03-29 17:45:10 +0200

    zadanie 3

---
+++ lab02zad03/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        KoszykProduktow koszyk = new KoszykProduktow();
+
+        koszyk.dodajProduktDoKoszyka("masło");
+        koszyk.dodajProduktDoKoszyka("mleko");
+        koszyk.dodajProduktDoKoszyka("chleb");
+
+        koszyk.pokazZamowienie();
+
+
+    }
+}
+
+class KoszykProduktow{
+    private LinkedList produkty = new LinkedList();
+
+    public void pokazZamowienie(){
+        System.out.println("Twój koszyk:");
+        System.out.println(produkty);
+    }
+
+    public void dodajProduktDoKoszyka(String produkt){
+        produkty.add(produkt);
+    }
+}
