6b3d7d5ff884a446625f93e4930741c1a4ad8d61
Justyna Krotoszynska <justyna.krotoszynska@gmail.com>
2021-05-25 20:21:56 +0200

    TestApp done

--- lab07/uzupelnienie2/src/com/company/zad10/TestApp.java
+++ lab07/uzupelnienie2/src/com/company/zad10/TestApp.java
@@ -1,4 +1,9 @@
 package com.company.zad10;
 
+import javax.swing.*;
+
 public class TestApp {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(RuchomySprite::new);
+    }
 }
