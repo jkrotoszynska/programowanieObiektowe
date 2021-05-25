5d782f60d7793498f99605e472b9b3f98244c56e
Justyna Krotoszynska <justyna.krotoszynska@gmail.com>
2021-05-25 20:43:39 +0200

    polowa Ruchomego Sprite

--- lab07/uzupelnienie2/src/com/company/zad10/RuchomySprite.java
+++ lab07/uzupelnienie2/src/com/company/zad10/RuchomySprite.java
@@ -1,4 +1,60 @@
 package com.company.zad10;
 
-public class RuchomySprite {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class RuchomySprite extends JFrame implements KeyListener, ActionListener {
+    JButton btnLewo;
+    JButton btnPrawo;
+    JButton btnGora;
+    JButton btnDol;
+    PanelGry p;
+
+    public RuchomySprite(){
+        super("Ruchomy Duszek");
+
+        p = new PanelGry()
+
+        JPanel btnPanel = new JPanel(new FlowLayout())
+
+        btnLewo = new JButton("Lewo ");
+        btnLewo.addActionListener(this);
+        btnPanel.add(btnLewo);
+
+        btnPrawo = new JButton("Prawo");
+        btnPrawo.addActionListener(this);
+        btnPanel.add(btnPrawo);
+
+        btnGora = new JButton("Gora");
+        btnGora.addActionListener(this);
+        btnPanel.add(btnGora);
+
+        btnDol = new JButton("Dol");
+        btnDol.addActionListener(this);
+        btnPanel.add(btnDol);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
